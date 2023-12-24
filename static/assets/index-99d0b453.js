a2782c258904aee7242a2d8b85788834fda8af9a
qusay <lonely.wq@gmail.com>
2023-12-24 17:30:17 +0800

    init

---
+++ static/assets/index-99d0b453.js
@@ -0,0 +1,1 @@
+import{q as t,aQ as a}from"./index-2c5cc3df.js";function o(){let e=localStorage.getItem("account_name");return t.jsxs("div",{children:["当前登录的用户:",e,t.jsx(a,{})]})}export{o as default};
