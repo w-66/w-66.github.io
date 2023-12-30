f92c6bf16e5121c5997b4a99e8aafca0eab4cb1b
qusay <lonely.wq@gmail.com>
2023-12-30 17:58:05 +0800

    HashRouter

---
+++ static/index-930eb422.js
@@ -0,0 +1,1 @@
+import{j as t,aG as a}from"./index-154b26d8.js";function o(){let e=localStorage.getItem("account_name");return t.jsxs("div",{children:["当前登录的用户:",e,t.jsx(a,{})]})}export{o as default};
